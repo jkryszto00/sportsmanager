f5a4fa82ab4d4bde93508f1b38672e51f00c42f4
jkryszto00 <jakubkryszto@gmail.com>
2025-12-14 10:26:13 +0100

    modules cms init

--- modules/CMS/routes/api.php
+++ modules/CMS/routes/api.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use modules\CMS\Http\Controllers\CMSController;
+use Modules\CMS\Http\Controllers\CMSController;
 
 Route::middleware(['auth:sanctum'])->prefix('v1')->group(function () {
     Route::apiResource('cms', CMSController::class)->names('cms');
--- modules/CMS/routes/web.php
+++ modules/CMS/routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use modules\CMS\Http\Controllers\CMSController;
+use Modules\CMS\Http\Controllers\CMSController;
 
 Route::middleware(['auth', 'verified'])->group(function () {
     Route::resource('cms', CMSController::class)->names('cms');
